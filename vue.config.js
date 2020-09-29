4f342527016333a7c9f4e1c839ce592795f1f3b0
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-09-28 22:46:10 -0700

    router setup

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `
+          @import "~@/styles/variables.scss";
+          @import "~@/styles/typography.scss";
+        `
+      }
+    }
+  }
 };
