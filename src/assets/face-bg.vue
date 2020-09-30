e3445a8954cd4fe88119b7939b1c6120402ce738
Clarence Adrian <clarenceadrian@alumni.ubc.ca>
2020-09-29 19:19:18 -0700

    add icons

---
+++ src/assets/face-bg.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="400"
+    height="450"
+    viewBox="0 0 400 450"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <rect width="400" height="450" fill="url(#paint0_linear)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x1="0"
+        y1="0"
+        x2="346.982"
+        y2="487.719"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#00887A" />
+        <stop offset="1" stop-color="#77A6F6" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
